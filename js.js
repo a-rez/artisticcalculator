9efc5c957642b4ef3bed369cf1bf154ad0416681
Atlas Rezhko <atlasrez@gmail.com>
2022-01-08 04:57:16 -0800

    Add files via upload

--- js.js
+++ js.js
@@ -1,11 +1,3 @@
-// var math = require('mathjs');
-
-// const { sqrt } = require("mathjs");
-
-// import { sqrt } from 'mathjs'
-// import('mathjs')
-console.log(math.sqrt(4));
-
 let red = "#830509";
 let orange = "#b03500";
 let yellow = "#b89602";
@@ -18,29 +10,9 @@ let purple = "#460354";
 let white = "#ffffff";
 let grey = "rgb(0, 0, 0)";
 
-let errormsg = "too many operators my feeble mind cannot keep up";
+let errormsg = "Error";
 
 
-// TOP told me to do this, I don't see why
-const add = function(a, b) {
-    return a+b;
-}
-
-const subtract = function(a, b) {
-    return a-b;
-}
-
-const multiply = function(a, b) {
-
-    // this works
-    console.log("from multiply: "+Number.parseFloat(a).toPrecision(2)*Number.parseFloat(b).toPrecision(2));
-    return Number.parseFloat(a).toPrecision(2)*Number.parseFloat(b).toPrecision(2);
-
-}
-
-const divide= function(a, b) {
-    return a/b;
-}
 
 
 // input: digit of button or result
@@ -103,6 +75,7 @@ function removeSpans() {
     list_of_dots = [];
 }
 
+
 function clearEL() {
 
     removeSpans();
@@ -111,15 +84,22 @@ function clearEL() {
 
 }
 
+function dotEL() {
+
+    stored[stored.length-1] += ".";
+    textbox.value += ".";
+}
+
 function plusEL() {
+    
     if (stored.length == 3 && stored[0] != "-") {
         enter();
     } else if (stored.length == 0){
         return;
     } else {
     stored.push("+");
-    textbox.value += "+";
     }
+    
 }
 
 function subtractEL() {
@@ -128,7 +108,6 @@ function subtractEL() {
    
     }else {
     stored.push("-");
-    textbox.value += "-";
     }
 }
 
@@ -139,7 +118,6 @@ function multiplyEL() {
         return;
     } else {
     stored.push("*");
-    textbox.value += "*";
     }
 }
 
@@ -151,30 +129,91 @@ function divideEL() {
     } else {
 
     stored.push("/");
-    textbox.value += "/";
+    }
+}
+
+function processInput(keyName) {
+    if (keyName == "Enter") {
+        enterEventListener();
+    }
+
+    else if (keyName == "Backspace") {
+        backspaceEventListener();
+    }
+    else if (keyName == ".") {
+        dotEL();
+    }
+    
+    else if (list_of_operators.includes(keyName)) {
+        list_of_operators_EL[list_of_operators.indexOf(keyName)]();
+    }
+
+    else if (list_of_nums.includes(keyName)) {
+        if (stored.length == 0) {
+            stored.push(keyName);
+        } else {
+
+            if (isNaN(parseInt(stored[stored.length-1]))) {
+                stored.push(keyName);
+            } else {
+                stored[stored.length-1] += keyName;
+            }
+        }
+
+        makeSpans(keyName);
+    
+  }
+
+}
+
+function processTextbox() {
+    input = textbox.value;
+    stored = [];
+
+    for (let i = 0;i < input.length; i++) {
+
+        processInput(input[i])
+
+
     }
 }
 
 const operate = function(operator, a, b) {
     switch(operator) {
         case "+":
-            return add(a, b).toFixed(2);
+            return round22(parseFloat(a) + parseFloat(b));
         case "-":
-            return subtract((a).toPrecision(2), (b).toPrecision(2)).toPrecision(2);
+            // return subtract((a).toPrecision(2), (b).toPrecision(2)).toPrecision(2);
+            return round22(parseFloat(a) - parseFloat(b));
         case "*":
-            return Number.parseFloat(a).toPrecision(2)*Number.parseFloat(b).toPrecision(2);
+            // return Number.parseFloat(a).toPrecision(2)*Number.parseFloat(b).toPrecision(2);
+            return round22(parseFloat(a)*parseFloat(b));
         case "/":
-            console.log("a: "+a);
-            console.log("b: "+b);        
-            return parseFloat(divide(a, b).toPrecision(2));
+            return round22(parseFloat(a)/parseFloat(b));
+            // return parseFloat(divide(a, b).toPrecision(2));
     }
 }
 
+
+function round22(number) {
+    return Math.round(((number+Number.EPSILON)*100))/100;
+}
+
 const enter = function() {
+
+
+
     removeSpans();
 
-    result = operate(stored[1], Number.parseFloat(stored[0]).toPrecision(2), Number.parseFloat(stored[2]).toPrecision(2));
-    console.log("prelim res "+ result);
+    
+
+    if (stored.length > 4) {
+        textbox.value = errormsg;
+        stored = [];
+        return;
+    }
+
+    result = operate(stored[1], stored[0], stored[2]);
 
     textbox.value = result;
 
@@ -199,15 +238,18 @@ const enterNegative = function() {
 
 
 function enterEventListener() {
+    if (stored.length == 1 || stored.length == 0 || stored.length == 2 /*&& list_of_nums.includes(parseInt(stored[0]))*/) {
+        return;
+    }
+
     removeSpans();
-    console.log(stored);
+
     // processing negative numbers
     for (let i = 0; i < stored.length; i++) {
         if (i == 0) {
             if (stored[0] == "-" && !isNaN(parseInt(stored[1]))) {
                 stored[1] = stored[0] + stored[1];
                 stored.shift();
-                console.log(stored);
 
             }
         }
@@ -216,106 +258,15 @@ function enterEventListener() {
             if (stored[i - 1] == "-" && isNaN(parseInt(stored[i - 2]))) {
                 stored[i] = "-" + stored[i];
                 stored.splice(i - 1, 1);
-                console.log(stored);
 
             }
 
         }
     }
-    // brace yourself
-    switch (stored.length) {
-        case 0:
-            stored = [];
-            textbox.value = [];
-            break;
-        case 1:
-            if (stored.length == 0 || list_of_operators.includes(stored[0])) {
-                stored = [];
-                textbox.value = "";
-            }
 
-            break;
-
-        case 2:
-            if ((typeof (parseInt(stored[0])) == "number") && ((stored[1] == "+") || (stored[1] == "-") || (stored[1] == "*") || (stored[1] == "/"))) {
-                // number and operator case
-                let result = operate(stored[1], parseFloat(stored[0]), parseFloat(stored[0]));
-                textbox.value = result;
-                stored.push(result);
-                makeSpans(result);
-                break;
-            }
-
-        case 4:
-            if (stored[1] == "+" && stored[2] == "-") {
-                stored[1] = stored[2];
-                stored[2] = stored[3];
-                stored.pop();
-                enter();
-
-            } else if (stored[1] == "-" && stored[2] == "-") {
-                stored[1] = "+";
-                stored[2] = stored[3];
-                stored.pop();
-                console.log("stored: " + stored);
-                enter();
-
-            } else if ((stored[1] == "*" || stored[1] == "/") && stored[2] == "-") {
-
-                stored[2] = stored[3];
-                stored.pop();
-                console.log("stored: " + stored);
-                enterNegative();
-
-            } else if (stored[0] = "-" && !isNaN(parseInt(stored[1])) && list_of_operators.includes(stored[2]) && !isNaN(parseInt(stored[3]))) {
-                stored[0] = stored[1];
-                stored[1] = stored[2];
-                stored[2] = stored[3];
-
-                enterNegative();
-
-            } else {
-
-                stored = [];
-                textbox.value = errormsg;
-            }
-            break;
-
-        case 5:
-            if (stored[0] == "-" && stored[3] == "-" && (stored[2] == "+" || stored[2] == "-")) {
-                console.log("here");
-
-                stored[0] = "-" + stored[1];
-                stored[1] = stored[2];
-                stored[2] = "-" + stored[4];
-                stored.pop();
-                stored.pop();
-                console.log("stored: " + stored);
-                enter();
-            }
-            if (stored[0] == "-" && stored[3] == "-" && (stored[2] == "*" || stored[2] == "/")) {
-
-                stored[0] = stored[1];
-                stored[1] = stored[2];
-                stored[2] = stored[4];
-                stored.pop();
-                stored.pop();
-                console.log("stored: " + stored);
-                enter();
-            }
-            break;
-        default:
-            enter();
-
-            // this doesn't
-            console.log(result);
-
-    }
-
-
-
-
+    enter();
 }
+
 
 const backspaceEventListener = function() {
     if (textbox.value == errormsg) {
@@ -323,11 +274,12 @@ const backspaceEventListener = function() {
         stored = [];
     }
     if (stored) {
-        stored[stored.length - 1] = (stored.at(-1) / 10 | 0);
+//        stored[stored.length - 1] = (stored.at(-1) / 10 | 0);
+        stored[stored.length - 1] = String(stored.at(-1)).slice(0, -1);
         if (stored[stored.length - 1] == 0) {
             stored.pop();
         }
-        textbox.value = textbox.value.slice(0, -1);
+//         textbox.value = textbox.value.slice(0, -1);
         if (list_of_dots.length) {
         list_of_dots.at(-1).remove();
         list_of_dots.pop();
@@ -335,47 +287,57 @@ const backspaceEventListener = function() {
     }
 }
 
-let keyName;
+// let keyName;
 let list_of_keys = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "+", "-", "/", "*"];
 let list_of_operators = ["+", "-", "*", "/"];
 let list_of_operators_EL = [plusEL, subtractEL, multiplyEL, divideEL];
 var list_of_nums = list_of_keys.filter(f => !list_of_operators.includes(f));
 
+
 document.addEventListener('keydown', (event) => {
+    keyName = event.key;
     // prevents double characters entered
+    
     if (event.target === textbox) {
+        if (keyName == "Enter") {
+            processTextbox();
+            enterEventListener();
+        }
         return;
     }
-    keyName = event.key;
+
+
     if (keyName == "Enter") {
         enterEventListener();
     }
 
     else if (keyName == "Backspace") {
         backspaceEventListener();
-    }
-    
-    else if (list_of_operators.includes(keyName)) {
-        list_of_operators_EL[list_of_operators.indexOf(keyName)]();
-    }
-
-    else if (list_of_nums.includes(keyName)) {
-        if (stored.length == 0) {
-            stored.push(keyName);
-        } else {
-
-            if (isNaN(parseInt(stored[stored.length-1]))) {
-                stored.push(keyName);
-            } else {
-                stored[stored.length-1] += keyName;
-            }
-        }
-
-        makeSpans(keyName);
-        textbox.value += keyName;
+        textbox.value = textbox.value.slice(0, -1);
         
     }
-});
+
+    else if (keyName == ".") {
+        dotEL();
+    }
+
+    if (list_of_operators.includes(keyName)) {
+        list_of_operators_EL[list_of_operators.indexOf(keyName)]();
+        if (stored.length == 1) {
+            return;
+        }
+
+        textbox.value += keyName;
+    }
+
+    else if (list_of_nums.includes(keyName)){
+
+        processInput(keyName);
+        textbox.value += keyName;
+    }
+        
+    }
+);
 
 // makes the front end
 let maindiv = document.createElement("div");
@@ -385,17 +347,29 @@ document.body.append(maindiv);
 let textboxdiv = document.createElement("div");
 textboxdiv.id = "textbox";
 let textbox = document.createElement("input");
+textbox.addEventListener("keydown", function (e) {
+    if (e.key == "Backspace") {
+        backspaceEventListener();
+        if (list_of_dots.length) {
+            list_of_dots.at(-1).remove();
+            list_of_dots.pop();
+            }
+    }
+
+});
+
 textboxdiv.append(textbox);
+
 maindiv.appendChild(textboxdiv);
 
 let buttons = [];
 let divs = [];
-let list = ["Clear", "Backspace", "Enter", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "/"];
+let list = [".","Clear", "Backspace", "Enter", "1", "2", "3", "+", "4", "5", "6", "-", "7", "8", "9", "*", "0", "/"];
 
-let colors = [grey, grey, grey, red, orange, yellow, grey, green1, green2, blue1, grey, blue2, blue3, purple, grey, white, grey];
+let colors = [grey, grey, grey, grey, red, orange, yellow, grey, green1, green2, blue1, grey, blue2, blue3, purple, grey, white, grey];
 
 let stored = [];
-let result = 0;
+let result = 0; 
 
 // takes a list of button labels and makes the buttons
 function makeButtonsFromList (list, divs, buttons, colors) {
@@ -421,49 +395,77 @@ function makeButtonsFromList (list, divs, buttons, colors) {
         } 
 
         else if (list[i] == "Backspace"){
-            buttons[i].addEventListener("click", backspaceEventListener)
+
+            buttons[i].addEventListener("click", function() {
+                backspaceEventListener();
+                textbox.value = textbox.value.slice(0, -1);
+            }
+                )
+
         }
 
         else if (list[i] == "+") {
-            buttons[i].addEventListener("click", plusEL)
+            buttons[i].addEventListener("click", function() {
+                plusEL();
+                if (stored.length == 1) {
+                    return;
+                }
+                textbox.value += "+";
+            });
         }
 
         else if (list[i] == "-") {
-            buttons[i].addEventListener("click", subtractEL);
+            buttons[i].addEventListener("click", function() {
+                subtractEL();
+                if (stored.length == 1) {
+                    return;
+                }
+                textbox.value += "-";
+            });
         }
 
         else if (list[i] == "*") {
-            buttons[i].addEventListener("click", multiplyEL);
+            buttons[i].addEventListener("click", function() {
+                multiplyEL();
+                if (stored.length == 1) {
+                    return;
+                }
+                textbox.value += "*";
+            });
         }
 
         else if (list[i] == "/") {
-            buttons[i].addEventListener("click", divideEL);
+            buttons[i].addEventListener("click", function () {
+                divideEL();
+                if (stored.length == 1) {
+                    return;
+                }
+                textbox.value += "/";
+            });
+        }
+
+        else if (list[i] == ".") {
+            buttons[i].addEventListener("click", function() {
+                dotEL();
+            })
         }
 
         else {
             buttons[i].addEventListener("click", function(){
-                if (stored.length == 0) {
-                    stored.push(list[i]);
-                } else {
-
-                    if (isNaN(parseInt(stored[stored.length-1]))) {
-                        stored.push(list[i]);
-                    } else {
-                        stored[stored.length-1] += list[i];
-                    }
-                }
-
-                makeSpans(list[i]);
+               
+                processInput(list[i]);
                 textbox.value += list[i];
+                
                 });
-    }
+        }
     }
 }
 
 makeButtonsFromList(list, divs, buttons, colors);
 
 // need to rename this manually because no element name can start with a /
-divs[16].id = "slash";
+divs[0].id = "dot";
+divs[17].id = "slash";
 
 
 
